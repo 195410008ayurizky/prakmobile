a231b3f124acf08793347d2fd3a8c71aae27b3b3
195410008ayurizky <ayurizky876@gmail.com>
2021-10-20 15:07:35 +0700

    TipTime

---
+++ TipTime/app/src/main/java/com/rizkyamalia/ayu/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.rizkyamalia.ayu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
